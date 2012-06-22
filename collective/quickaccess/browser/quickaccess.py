ec5b3ab4bff1ac99b4bd86d89a987a51bc39c8a0
Tristan Le Toullec <tristan.letoullec@gmail.com>
2012-06-22 07:32:10 +0200

    First functionnal version. That do the job

--- collective/quickaccess/browser/quickaccess.py
+++ collective/quickaccess/browser/quickaccess.py
@@ -9,29 +9,57 @@ from z3c.form import form , button , error
 from z3c.form.interfaces import ActionExecutionError, WidgetActionExecutionError
 from plone.z3cform import layout
 from zope.annotation.interfaces import IAttributeAnnotatable, IAnnotations
+from Acquisition import aq_inner, aq_parent
 
 from collective.quickaccess.annotationmgr import IAnnotationManager
+
 
 class bar(ViewletBase):
     render = ViewPageTemplateFile('quickaccess.pt')
     def update(self):
             an = IAnnotationManager(self.context)
-            self.activate=True
+            content=self.readQA(self.context)
+            if content['qatext'] == None:
+                content['qatext'] = ''
+            if content['qalinks'] == None:
+                content['qalinks'] = ''
+            if content['qalinks'] != '':
+                self.activate=True
+                self.qaLinksList=[]
+                self.qaherit=content['qaherit']
+                self.qatext=content['qatext']
+                for linkline in content['qalinks'].split("\n"):
+                    link=linkline.split(';')[0]
+                    url=linkline.split(';')[1]
+                    self.qaLinksList.append({'link':link,'url':url})
+            else:
+                self.activate=False
+
+    def readQA(self, object2read):
+            an = IAnnotationManager(object2read)
+            isAnnotation=True
             try:
                 content = an.read()
             except:
-                content={'qablock':False,'qatext':'','qalinks':''}
-            self.qaLinksList=[]
-            self.qablock=content['qablock']
-            self.qatext=content['qatext']
-            for linkline in content['qalinks'].split("\n"):
-                link=linkline.split(';')[0]
-                url=linkline.split(';')[1]
-                self.qaLinksList.append({'link':link,'url':url})
+                isAnnotation=False
+                
+            if isAnnotation:
+                # CAS d'arrive au repertoire racine
+                if object2read.id =='Plone' and content['qalinks'] != '':
+                    return content
+                if content['qaherit'] == True:
+                    return self.readQA(aq_inner(object2read).aq_parent)
+                else:
+                    return content
+            else:
+                if object2read.id =='Plone':
+                    content={'qaherit':False,'qatext':'','qalinks':''}
+                    return content
+                return self.readQA(aq_inner(object2read).aq_parent)
 
 class IQuickAccessManage(interface.Interface):
     """metadata form"""
-    qablock = schema.Bool(title=u"Herit", description=u"Herit QuickAccess in this section (and childrens)",default=True)
+    qaherit = schema.Bool(title=u"Herit", description=u"Herit QuickAccess in this section (and childrens)",default=True)
     qatext = schema.TextLine(title=u"Text", description=u"Text to introduce quickaccess bar",
                               required=False)
     qalinks = schema.Text(title=u"Links", description=u"Please enter one link per line, separate link and URL by semicolon (;)",
@@ -49,7 +77,7 @@ class QaManage(AutoExtensibleForm , form.Form):
         try:
             content = an.read()
         except:
-            content={'qablock':'False','qatext':'','qalinks':''}
+            content={'qaherit':'False','qatext':'','qalinks':''}
         return content
 
     @button.buttonAndHandler(u"Save")
